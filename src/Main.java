66beb87ab4c4a6c2e11cc95cd95089a9aef18941
MlepNos <72391873+MlepNos@users.noreply.github.com>
2023-05-31 20:44:25 +0200

    second question

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //Vehicle vehicle = new Vehicle(8, "Truck");
-        //CreateVehicle(vehicle);
 
-        //Vehicle mitsubishi = new Mitsubishi(10,"Race Car");
-        //CreateVehicle(mitsubishi);
 
-        //or
-        Vehicle car = new Vehicle(8, "Base car");
-        car.startEngine();
-        car.accelerate();
-        car.brake();
 
-        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD");
-        mitsubishi.startEngine();
-        mitsubishi.accelerate();
-        mitsubishi.brake();
 
-        Ford ford = new Ford(6, "Ford Falcon");
-        ford.startEngine();
-        ford.accelerate();
-        ford.brake();
-
-        Holden holden = new Holden(6, "Holden Commodore");
-        holden.startEngine();
-        holden.accelerate();
-        holden.brake();
     }
 
 
@@ -125,4 +103,122 @@ The other three classes mentioned above are sub-classes of Vehicles.
 These classes have no member variables and the constructor for each will call the parent constructor for object instantiation.
 Each of these classes will override the three parent methods startEngine, accelerate, and brake.
 The return messages for these methods should somewhere contain the name of the class to which the methods belong.
+
+
+INPUT EXAMPLE:
+       Vehicle vehicle = new Vehicle(8, "Truck");
+       CreateVehicle(vehicle);
+       Vehicle mitsubishi = new Mitsubishi(10,"Race Car");
+       CreateVehicle(mitsubishi);
+
+        //or
+        Vehicle car = new Vehicle(8, "Base car");
+        car.startEngine();
+        car.accelerate();
+        car.brake();
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD");
+        mitsubishi.startEngine();
+        mitsubishi.accelerate();
+        mitsubishi.brake();
+
+        Ford ford = new Ford(6, "Ford Falcon");
+        ford.startEngine();
+        ford.accelerate();
+        ford.brake();
+
+        Holden holden = new Holden(6, "Holden Commodore");
+        holden.startEngine();
+        holden.accelerate();
+        holden.brake();
+
+
+       Challange 3
+
+Bill runs a fast food hamburger restaurant, and sells hamburger meals.His meal orders are composed of three items, the hamburger, the drink, and the side item.
+Your application lets Bill select the type of burgers, and some of the additional items, or extras, that can be added to the burgers, as well as the actual pricing.
+
+You need a meal order.This should be composed of exactly one burger, one drink, and one side item.
+The most common meal order should be created without any arguments, like a regular burger, a small coke, and fries, for example.
+You should be able to create other meal orders, by specifying different burgers, drinks, and side items.
+You need a drink, and side item.
+The drink should have at least a type, size and price, and the price of the drink should change for each size.
+The side item needs at least a type and price.
+
+You need burgers.Every hamburger should have a burger type, a base price, and up to a maximum of three extra toppings.
+The constructor should include only the burger type and price.
+Extra Toppings on a burger need to be added somehow, and priced according to their type.
+The deluxe burger bonus.
+Create a deluxe burger meal, with a deluxe burger, that has a set price, so that any additional toppings do not change the price.
+The deluxe burger should have room for an additional two toppings.
+
+Your main method should have code to do the following:
+Create a default meal, that uses the no arguments constructor.
+Create a meal with a burger, and the drink and side item of your choice, with up to 3 extra toppings.
+Create a meal with a deluxe burger, where all items, drink, side item and toppings up to 5 extra toppings, are included in the burger price.
+
+For each meal order, you'll want to perform these functions:
+Add some additional toppings to the burger.
+Change the size of the drink.
+Print the itemized list. This should include the price of the burger, any extra toppings, the drink price based on size, and the side item price.
+Print the total due amount for the meal.
+
+The diagram doesn't include the DeluxeBurger class.  We'll look at that a bit later.
+The MealOrder class uses composition in this design. It's composed of a burger, as well as drink and side, which will just be Items.
+I've used inheritance for the Item and Burger relationships, which means Burger is an Item.
+Every Item has a name, type, price or base price, and a size.
+
+Item has the method getBasePrice, which is really just a getter method for the price, but the name is more descriptive.
+Item also has getAdjustedPrice, and the printItem method.
+These methods will exhibit different behavior, based on the runtime type, and we know that's polymorphism.
+
+For the burger, the toppings or extras are individual attributes, and also have the type Item.
+We're going to use the MealOrder class, to hide some of the implementation details from the calling code.
+This means we're going to use encapsulation techniques on MealOrder and Item.
+
+EXAMPLE INPUT:
+      MealOrder regularMeal = new MealOrder();
+      regularMeal.printItemizedList();
+      System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+      regularMeal.addBurgerToppings("BACON","CHEESE","MAYO");
+      regularMeal.printItemizedList();
+      System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+      MealOrder secondMeal = new MealOrder("turkey","7-up","chili");
+      secondMeal.addBurgerToppings("LETTUCE","CHEESE","MAYO");
+      secondMeal.setDrinkSize("SMALL");
+      secondMeal.printItemizedList();
+
+ EXAMPLE OUTPUT:
+ BASE BURGER:  4.00
+------------------------------
+      REGULAR BURGER:  4.00
+         MEDIUM COKE:  1.00
+        MEDIUM FRIES:  1.50
+------------------------------
+         TOTAL PRICE:  6.50
++++++++++++++++++++++++++++++++++++++++++++++++++++++
+         BASE BURGER:  4.00
+               BACON:  1.50
+              CHEESE:  1.00
+                MAYO:  0.00
+------------------------------
+      REGULAR BURGER:  6.50
+         MEDIUM COKE:  1.00
+        MEDIUM FRIES:  1.50
+------------------------------
+         TOTAL PRICE:  9.00
++++++++++++++++++++++++++++++++++++++++++++++++++++++
+         BASE BURGER:  4.00
+             LETTUCE:  0.00
+              CHEESE:  1.00
+                MAYO:  0.00
+------------------------------
+       TURKEY BURGER:  5.00
+          SMALL 7-UP:  0.50
+        MEDIUM CHILI:  1.50
+------------------------------
+         TOTAL PRICE:  7.00
+
   */
